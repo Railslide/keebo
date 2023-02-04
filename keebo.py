6463ee171f4a23cd8bfba783fce0b691a59f956b
Giulia Vergottini <hello@railslide.io>
2023-02-04 18:07:04 +0100

    Handle positional keys

--- keebo.py
+++ keebo.py
@@ -2,37 +2,58 @@ import collections
 import json
 import re
 
+from typing import Dict, Optional
+
 import keyboard
 
-used_keys: collections.Counter = collections.Counter()
 
-def get_positional_keys_mapping():
-    mapping = {}
-    with open("/usr/include/linux/input-event-codes.h") as fh:
-        keycode_definitions = fh.read()
+class KeyCounter:
+    def __init__(self):
+        self.positional_keys = KeyCounter._get_positional_keys_mapping()
+        self._used_keys = collections.Counter()
 
-    pattern = "#define\s+KEY_((?:LEFT|RIGHT)(?:CTRL|SHIFT))\s+(\d+)"
-    for key, scan_code in re.findall(pattern, keycode_definitions, re.MULTILINE):
-        mapping[scan_code] = key.lower()
-    return mapping
+    @classmethod
+    def _get_positional_keys_mapping(cls) -> Dict[int, str]:
+        """
+        Returns a `key: scan_code` mapping of positional key for shift and ctrl.
+        """
+        mapping = {}
+        with open("/usr/include/linux/input-event-codes.h") as fh:
+            keycode_definitions = fh.read()
 
+        pattern = "#define\s+KEY_((?:LEFT|RIGHT)(?:CTRL|SHIFT))\s+(\d+)"
+        for key, scan_code in re.findall(pattern, keycode_definitions, re.MULTILINE):
+            mapping[int(scan_code)] = key.lower()
+        return mapping
 
-def register_key(key: keyboard.KeyboardEvent) -> None:
-    if key.name in ("ctrl", "shift"):
-        used_keys.update([f"{key.name}_{key.scan_code}"])
-    else:
-        used_keys.update([key.name])
-    print(used_keys)
+    def register_key(self, key: keyboard.KeyboardEvent) -> None:
+        # The keyboard library doesn't discern between left and right ctrl/shift
+        if key.name in ("ctrl", "shift"):
+            try:
+                pressed_key = self.positional_keys[key.scan_code]
+            except KeyError:
+                print(f"Couldn't figure out if it was left or right {key.name}")
+                pressed_key = key.name
+            self._used_keys.update([pressed_key])
+        else:
+            self._used_keys.update([key.name])
+        print(self._used_keys)
+
+    @property
+    def used_keys(self):
+        return self._used_keys
+
 
 def main() -> None:
-    keyboard.on_press(register_key)
+    key_counter = KeyCounter()
+    keyboard.on_press(key_counter.register_key)
 
     try:
         keyboard.wait()
     except KeyboardInterrupt:
         print("Saving before exiting..")
         with open("key_stats.json", "w") as fh:
-            fh.write(json.dumps(dict(used_keys)))
+            fh.write(json.dumps(dict(key_counter.used_keys)))
         print("Done! Byeeee")
         exit()
 
